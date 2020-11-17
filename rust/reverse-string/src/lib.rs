c9b396cb1039814ed27deac2c070354aad0b5282
Dan Fornika <dfornika@gmail.com>
2020-11-17 08:22:53 -0800

    Refine rust reverse-string soln based on mentor feedback

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,10 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut output = String::from("");
-    let g = input.graphemes(true).collect::<Vec<&str>>();
-    for s in g.iter().rev() {
-        output.push_str(s);
-    }
-    output
+    input.graphemes(true).rev().collect()
 }
