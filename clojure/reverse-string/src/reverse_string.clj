5a4855dce1258327b15112bbb9127cf26e7ef45f
Dan Fornika <dfornika@gmail.com>
2020-11-20 17:33:57 -0800

    Update soln for clojure/reverse-string

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,17 +1,5 @@
 (ns reverse-string)
 
-(defn reverse' [s]
-  (into '() s))
-
 (defn reverse-string [s]
-  (reduce str "" (reverse' s)))
+  (apply str (into '() s)))
 
-(comment
-  (butlast "hello")
-  (reverse' "hello")
-  (rseq (vec "hello"))
-  (reverse "hello")
-  (reverse-string "hello")
-  (reverse-string "I")
-  (into '() "hello")
-  )
